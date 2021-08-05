091a4b52e971fab08e2762fa97908329a472baff
Radhika <radhika.shankar1220@gmail.com>
2021-08-05 13:24:47 +0530

    [radhika] Retrieve record based on product id and rating

--- TestValidation/UnitTest1.cs
+++ TestValidation/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ProductReviewManagement;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TestValidation
 {
@@ -31,9 +32,18 @@ namespace TestValidation
         public void TestMethodForRetrieveTopThreeRecord()
         {
             int expected = 3;
-
             var actual = ProductReviewManager.RetrieveTopThreeRating(productList);
             Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        /// UC3-->Retrieve  records from list based on productid and rating > 3 
+        /// </summary>
+        [TestMethod]
+        public void TestMethodForRetrieveRecordsBasedOnRatingAndProductId()
+        {
+            int[] expected = { 1, 1, 4, 9, 9, 1, 4 };  
+            var actual = ProductReviewManager.RetrieveRecordsBasedOnRatingAndProductId(productList);
+            CollectionAssert.AreEqual(expected, actual);
         }
     }
 }
