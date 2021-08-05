a9f2cb2b3447d6c9a3aa4904cc252cb588e12b36
Radhika <radhika.shankar1220@gmail.com>
2021-08-05 08:23:44 +0530

    [Radhika] Adding details in list

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prodcut Review Management!!!!!!!!");
+            Console.WriteLine("Adding a Prodcut Review In list");
+            int option = Convert.ToInt32(Console.ReadLine());
+            //Creating a list for Product Review
+            List<ProductReview> productReviews = new List<ProductReview>();
+            switch(option)
+            {
+                case 1:
+                    ProductReviewManager.AddingProductReview(productReviews);
+                    break;
+            }
+        }
+    }
+}
